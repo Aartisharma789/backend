6a0e412a811093a5f5729dfc390df6bef6062c73
Mohamad Anas <mohamadanas0501@gmail.com>
2024-04-07 13:42:39 +0530

    Updated the / route in the app.js

--- app.js
+++ app.js
@@ -34,17 +34,9 @@ app.use('/api/v1', payment);
 
 // Deployment
 __dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, '/frontend/build')))
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-	});
-} else {
-	app.get('/', (req, res) => {
-		res.send('Server is Running! 🚀');
-	});
-}
+app.get('/', (req, res) => {
+	res.send('Server is Running! 🚀');
+});
 
 // Middleware for Errors
 app.use(errorMiddleware);
