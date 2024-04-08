ab968341b82dc18edbdb1d9d036a50a02c5b522c
Mohamad Anas <mohamadanas0501@gmail.com>
2024-04-08 22:20:11 +0530

    Added Cors in the backend

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
 const errorMiddleware = require('./middlewares/error');
 const morgan = require('morgan');
+const cors = require('cors');
 
 const app = express();
 
@@ -15,6 +16,7 @@ if (process.env.NODE_ENV !== 'production') {
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(fileUpload());
